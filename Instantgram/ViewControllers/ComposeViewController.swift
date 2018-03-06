bb6e08a6ab70125c996c1b6b416e0d0faa12997d
sks3 <saketsingh2018@gmail.com>
2018-03-05 21:09:53 -0600

    Enable camera

--- Instantgram/ViewControllers/ComposeViewController.swift
+++ Instantgram/ViewControllers/ComposeViewController.swift
@@ -8,14 +8,52 @@
 
 import UIKit
 
-class ComposeViewController: UIViewController {
+class ComposeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
+      let vc = UIImagePickerController()
+  
+  @IBOutlet var composeImage: UIImageView!
   
   override func viewDidLoad() {
         super.viewDidLoad()
 
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGesture:)))
+    composeImage.isUserInteractionEnabled = true
+    composeImage.addGestureRecognizer(tapGesture)
+    
+    vc.delegate = self
+    vc.allowsEditing = true
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      print("Camera is available 📸")
+      vc.sourceType = .camera
+    } else {
+      print("Camera 🚫 available so we will use photo library instead")
+      vc.sourceType = .photoLibrary
+    }
+    
+
         // Do any additional setup after loading the view.
     }
+  
+  
+  @objc func imageTapped(tapGesture: UITapGestureRecognizer) {
+    print("camera activated")
+    self.present(vc, animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController,
+                             didFinishPickingMediaWithInfo info: [String : Any]) {
+    // Get the image captured by the UIImagePickerController
+    let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+    let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+    
+    // Do something with the images (based on your use case)
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    dismiss(animated: true, completion: nil)
+  }
+  
+  
   
   @IBAction func cancelButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)  }
